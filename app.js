c9b6c64f303d6aa39938d89a7f6f1ca53e3a6ac2
Humble-Preacher <61693488+Humble-Preacher@users.noreply.github.com>
2023-02-22 01:01:30 +0000

    setup done

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
-(function (){
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
-    let screen = document.querySelector('.screen');
-    let buttons = document.querySelectorAll('.btn');
-    let equal = document.querySelector('.btn-equal');
-    let clear = document.querySelector('.btn-clear');
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
 
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function(e) {
-            let value = e.target.dataset.num;
-            screen.value += value;
-        })
-    });
-
-    equal.addEventListener('click', function(e) {
-        if (screen.value === '') {
-            screen.value = "Please enter a value";
-        }else {
-            let answer = eval(screen.value);
-            screen.value = answer;
-        }
-            
-    });
-
-    clear.addEventListener('click', function(e) {
-        screen.value = '';
-    })
-
-})();
